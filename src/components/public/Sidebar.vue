8b28790ac94684f8062733e6fe7a0c093a450e06
rifkyNur <ythrifky619@gmail.com>
2025-07-05 11:27:07 +0700

    fix sidebar height

--- src/components/public/Sidebar.vue
+++ src/components/public/Sidebar.vue
@@ -1,6 +1,13 @@
 <script setup>
 import { RouterLink } from "vue-router";
-import { HomeIcon, ChartPieIcon, WalletIcon, ShoppingCartIcon, UserIcon, ListBulletIcon } from "@heroicons/vue/24/solid";
+import {
+  HomeIcon,
+  ChartPieIcon,
+  WalletIcon,
+  ShoppingCartIcon,
+  UserIcon,
+  ListBulletIcon,
+} from "@heroicons/vue/24/solid";
 import { useToggleSidebarStore } from "../../stores/toggleSidebar";
 import { computed, onMounted, onBeforeUnmount, ref } from "vue";
 import { useAuthStore } from "@/stores/auth";
@@ -19,7 +26,11 @@ const sidebarIsOpen = computed(() => {
 const handleClickOutside = (event) => {
   const buttonToggle = document.getElementById("button-toggle");
 
-  if (sidebarRef.value && !sidebarRef.value.contains(event.target) && !buttonToggle.contains(event.target)) {
+  if (
+    sidebarRef.value &&
+    !sidebarRef.value.contains(event.target) &&
+    !buttonToggle.contains(event.target)
+  ) {
     toggleSidebar.closeSidebar();
   }
 };
@@ -34,43 +45,67 @@ onBeforeUnmount(() => {
 <template>
   <div
     ref="sidebarRef"
-    class="p-6 bg-SidebarLight backdrop-blur-md w-fit border-r border-black/5 min-h-screen origin-left absolute transition-all duration-300 z-10 md:static dark:bg-secondary text-white/70"
+    class="p-6 bg-SidebarLight backdrop-blur-md w-fit border-r border-black/5 min-h-screen h-full origin-left absolute transition-all duration-300 z-10 md:static dark:bg-secondary text-white/70"
     :class="sidebarIsOpen ? 'left-0' : '-left-full'"
   >
     <h2 class="text-xl font-bold mb-8 text-white">Menu</h2>
     <ul class="flex flex-col gap-6 text-white">
       <li>
-        <router-link to="/" active-class="text-blue-500" class="flex items-center gap-3">
+        <router-link
+          to="/"
+          active-class="text-blue-500"
+          class="flex items-center gap-3"
+        >
           <HomeIcon class="size-5" />
           <p class="font-semibold">Dasboard</p>
         </router-link>
       </li>
       <li>
-        <router-link to="/schedule" exact-active-class="text-blue-500" class="flex items-center gap-3">
+        <router-link
+          to="/schedule"
+          exact-active-class="text-blue-500"
+          class="flex items-center gap-3"
+        >
           <ListBulletIcon class="size-5" />
           <p class="font-semibold">Jadwal</p>
         </router-link>
       </li>
       <li>
-        <router-link to="/income" exact-active-class="text-blue-500" class="flex items-center gap-3">
+        <router-link
+          to="/income"
+          exact-active-class="text-blue-500"
+          class="flex items-center gap-3"
+        >
           <WalletIcon class="size-5" />
           <p class="font-semibold">Pemasukan</p>
         </router-link>
       </li>
       <li>
-        <router-link to="/expense" active-class="text-blue-500" class="flex items-center gap-3">
+        <router-link
+          to="/expense"
+          active-class="text-blue-500"
+          class="flex items-center gap-3"
+        >
           <ShoppingCartIcon class="size-5" />
           <p class="font-semibold">Pengeluaran</p>
         </router-link>
       </li>
       <li>
-        <router-link to="/recap" active-class="text-blue-500" class="flex items-center gap-3">
+        <router-link
+          to="/recap"
+          active-class="text-blue-500"
+          class="flex items-center gap-3"
+        >
           <ChartPieIcon class="size-5" />
           <p class="font-semibold">Rekapitulasi</p>
         </router-link>
       </li>
       <li v-if="!isLogin">
-        <router-link to="/login" active-class="text-blue-500" class="flex items-center gap-3">
+        <router-link
+          to="/login"
+          active-class="text-blue-500"
+          class="flex items-center gap-3"
+        >
           <UserIcon class="size-5" />
           <p class="font-semibold">Login</p>
         </router-link>
@@ -80,7 +115,10 @@ onBeforeUnmount(() => {
           <UserIcon class="size-5" />
           <p class="font-semibold">logout</p>
         </button>
-        <ModalLogout :isOpen="authStore.modalLogoutIsOpen" @close="authStore.closeModalLogout" />
+        <ModalLogout
+          :isOpen="authStore.modalLogoutIsOpen"
+          @close="authStore.closeModalLogout"
+        />
       </li>
     </ul>
   </div>
